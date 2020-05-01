e9184de6b5dfda125f533ed73e87e09313ef1a5e
Isaac_andre <andreisaac344@gmail.com>
2020-05-01 18:24:10 -0300

    BarbeariaApp

---
+++ src/main/java/com/barbeariaapp/barbeariaapp/BarbeariaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.barbeariaapp.barbeariaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/barbeariaapp/barbeariaapp/BarbeariaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbeariaapp.barbeariaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
